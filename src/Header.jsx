274b2fc2f293fd3e55352c4bd5db77eed81735ef
John <suttontaj13@gmail.com>
2022-03-17 16:43:49 -0400

    Added styling to header

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,25 +1,21 @@
-import React from "react";
-import "./Header.css";
+import React from 'react';
+import './Header.css';
 
-import { BsList, BsQuestionCircle } from "react-icons/bs";
-import { BiBarChartAlt2 } from "react-icons/bi";
-import { GoGear } from "react-icons/go";
+import { BsList, BsQuestionCircle } from 'react-icons/bs';
+import { BiBarChartAlt2 } from 'react-icons/bi';
+import { GoGear } from 'react-icons/go';
 
 function Header() {
   return (
     <header className="header">
       <div className="header-left">
-        <span>
-          <BsList />
-          <BsQuestionCircle />
-        </span>
+        <BsList size={25} style={{ 'stroke-width': 0.75 }} />
+        <BsQuestionCircle size={20} />
       </div>
       <div className="title">Wordle</div>
       <div className="header-right">
-        <span>
-          <BiBarChartAlt2 />
-          <GoGear />
-        </span>
+        <BiBarChartAlt2 size={25} />
+        <GoGear size={25} />
       </div>
     </header>
   );
